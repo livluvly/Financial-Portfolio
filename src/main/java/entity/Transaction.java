6b8b810c76dee82fb2913ca9034aca28f145aee7
livluvly <124837707+livluvly@users.noreply.github.com>
2024-11-30 16:08:57 -0500

    refactored to resolve conflicts

--- src/main/java/entity/Transaction.java
+++ src/main/java/entity/Transaction.java
@@ -1,67 +1,88 @@
 package entity;
 
+import java.util.Date;
+
 public class Transaction {
-    private final String date;
-    private String symbol;
-    private String name;
-    private int quantity;
-    private double totalValue;
-    private String type; // Either "BUY" or "SELL"
+    private final Date date;
+    private final String symbol;
+    private final String name;
+    private final int quantity;
+    private final double totalCost;
+    private final String type;
 
+    /**
+     * Constructor for Transaction Class.
+     * @param symbol The stock symbol.
+     * @param name The name of the stock.
+     * @param quantity The number of units.
+     * @param date The date of the transaction.
+     * @param totalCost The total cost of the transaction.
+     * @param type The type of transaction: "BUY" or "SELL".
+     * @throws IllegalArgumentException If the transaction type is not "BUY" or "SELL".
+     * @throws NullPointerException If symbol and date are null.
+     */
+    public Transaction(String symbol, String name, int quantity, Date date, double totalCost, String type) {
+        // Transaction type should either be "BUY" or "SELL"
+        if (!"BUY".equals(type) && !"SELL".equals(type)) {
+            throw new IllegalArgumentException("Invalid transaction type, must be either 'BUY' or 'SELL'.");
+        }
 
-    public Transaction(String symbol, String name, int quantity, String date, double totalValue, String type) {
-        this.date = date; // Format:  YYYY-MM-DD
+        if (symbol == null || date == null) {
+            throw new NullPointerException("Symbol and date cannot be null.");
+        }
+        this.date = date;
         this.symbol = symbol;
         this.name = name;
         this.quantity = quantity;
-        this.totalValue = totalValue;
+        this.totalCost = totalCost;
         this.type = type;
     }
 
-    public String getDate() {
+    /**
+     * Getter for the transaction date.
+     * @return the date of the transaction.
+     */
+    public Date getDate() {
         return date;
     }
 
-    public String setDate(String date) {
-        return date;
-    }
-
+    /**
+     * Getter for the stock symbol.
+     * @return the stock symbol.
+     */
     public String getSymbol() {
         return symbol;
     }
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
+    /**
+     * Getter for the name of the stock.
+     * @return the full name of the stock.
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String name) { this.name = name; }
-
+    /**
+     * Getter for the quantity of units.
+     * @return the number of units.
+     */
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    /**
+     * Getter for the total cost of the transaction.
+     * @return the total cost.
+     */
+    public double getTotalCost() {
+        return totalCost;
     }
 
-    public double getTotalValue() {
-        return totalValue;
-    }
-
-    public void setTotalValue(double totalValue) {
-        this.totalValue = totalValue;
-    }
-
+    /**
+     * Getter for the transaction type.
+     * @return  the type of transaction ("BUY" or "SELL").
+     */
     public String getType() {
         return type;
     }
-
-    public void setTotalValue(String type) {
-        this.type = type;
-    }
-
 }
